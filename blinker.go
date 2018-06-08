af798478f9f9523d3bd6ce88c3790520fba71769
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-06-09 01:37:44 +0530

    Adding blinker.go from https://gobot.io/documentation/platforms/raspi/.

---
+++ blinker.go
@@ -0,0 +1,29 @@
+// Code from: https://gobot.io/documentation/platforms/raspi/
+package main
+
+import (
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+)
+
+func main() {
+	r := raspi.NewAdaptor()
+	led := gpio.NewLedDriver(r, "7")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("blinkBot",
+		[]gobot.Connection{r},
+		[]gobot.Device{led},
+		work,
+	)
+
+	robot.Start()
+}
